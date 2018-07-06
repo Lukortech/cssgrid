aca2054308e8f7a9e12d7d809c8340e837ec2796
Lukortech <lukortech@gmail.com>
2018-07-06 14:33:14 +0200

    Calc2

---
+++ scripts.js
@@ -0,0 +1,13 @@
+var set = true;
+var num1 = undefined;
+var num2 = undefined;
+function number(){
+    console.log(this.innerHTML);
+}
+//if(document.getElementById("a").value !== 0){
+        
+//}
+/*document.getElementById("a").value = "1";
+function chose_number_two(num){
+    document.getElementById("b").value = "1";
+}*/
